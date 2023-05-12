387334216417061033da42e948d88047731f426b
vlad <khalvlalove@gmail.com>
2023-05-12 15:08:22 +0400

    add precondition

--- tests/test_pdf_task.py
+++ tests/test_pdf_task.py
@@ -1,12 +1,17 @@
 from controls import parser
 from erp_aero_test_task import pdf_page
+from utils.paths import resource
 
 
-def test_pdf_comparison(pdf_path):
+def test_pdf_comparison():
+    # GIVEN
+    path = resource('test_task.pdf')
+
     # WHEN
-    data = parser.pdf_to_dict(pdf_path)
+    data = parser.pdf_to_dict(path)
 
     # THEN
     pdf_page.check_keys(data)
-    pdf_page.verify_barcodes(pdf_path, data)
-    pdf_page.pdf_structure_is_valid(pdf_path)
+    pdf_page.verify_barcodes(path, data)
+    pdf_page.pdf_structure_is_valid(path)
+
